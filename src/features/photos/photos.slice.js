e33005f8473862341b77f782d60740c0bb471f46
Vishal Modi <vishalrmodi@gmail.com>
2023-12-20 17:06:25 -0800

    Add/delete/search dog(s)

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -1,22 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { selectSearchTerm } from '../search/search.slice';
-import photos from './photos.data.js';
+import { createSlice } from "@reduxjs/toolkit";
+import { selectSearchTerm } from "../search/search.slice";
+import photos from "./photos.data.js";
 
 const initialState = {
   photos,
 };
 
 const options = {
-  name: 'photos',
+  name: "photos",
   initialState,
   reducers: {
-    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
-    // Task 1 Hint: You can use state.photos.unshift()
-    // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
-    // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
-    // Task 6 Hint: You can use state.photos.splice()
-    // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    addPhoto: (state, action) => {
+      state.photos.push({
+        id: state.photos.length + 1,
+        caption: action.payload.caption,
+        imageUrl: action.payload.imageUrl,
+      });
+    },
+    removePhoto: (state, action) => {
+      state.photos.splice(
+        state.photos.findIndex((photo) => photo.id === action.payload),
+        1
+      );
+    },
   },
 };
 
@@ -28,5 +34,10 @@ export default photosSlice.reducer;
 
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
-  // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+  const allPhotos = selectAllPhotos(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return allPhotos.filter((photo) =>
+    photo.caption.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 };
